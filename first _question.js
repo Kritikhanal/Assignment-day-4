697020ff2cedc8fca7bd40cf139df33ab36c9831
kritikhanal <kkritima@outlook.com>
2023-07-05 23:25:16 +0545

    committed

---
+++ first _question.js
@@ -0,0 +1,39 @@
+
+
+function getSumPromise(numbers) {
+  return new Promise((resolve, reject) => {
+    if (numbers.length === 0) {
+      reject("No numbers provided");
+    } else {
+      const sum = numbers.reduce((a, b) => a + b);
+      resolve(sum);
+    }
+  });
+}
+
+// const numbers = [1, 2, 3, 4];
+// getSumPromise(numbers)
+//   .then(sum => {
+//     console.log('Sum:', sum);
+//   })
+//   .catch(error => {
+//     console.log('Error:', error);
+//   });
+
+async function getSumAsync(numbers) {
+  if (numbers.length === 0) {
+    throw new Error("No numbers provided");
+  } else {
+    return numbers.reduce((a, b) => a + b);
+  }
+}
+
+const numbers = [1, 2, 3, 4];
+(async () => {
+  try {
+    const sum = await getSumAsync(numbers);
+    console.log("Sum:", sum);
+  } catch (error) {
+    console.log("Error:", error.message);
+  }
+})();
---
+++ index.js
@@ -0,0 +1,29 @@
+
+
+//question no 2
+
+// displaying the data from api
+function processdata(data) {
+  
+  const id = document.getElementById("id");
+  const name = document.getElementById("name");
+  const price = document.getElementById("price");
+  const img = document.getElementById("img");
+  id.innerHTML = data[0].id;
+  name.innerHTML = data[0].name;
+  price.innerHTML = data[0].price;
+  img.src = data[0].image;
+  console.log(data[0].image);
+}
+
+const Demo_url = "https://course-api.com/react-store-products";
+const request = fetch(Demo_url)
+  .then((response) => {
+    console.log(response);
+    return response.json();
+  })
+  .then(processdata)
+  .catch((error) => {
+    console.log("error", error);
+  });
+console.log(request);
